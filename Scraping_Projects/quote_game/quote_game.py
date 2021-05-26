d2194b708759954d3eae13fd09e45d364b1e7d19
Tihomira Nikolova <tiholova8@gmail.com>
2021-05-26 18:55:14 +0200

    info added

--- Scraping_Projects/quote_game/quote_game.py
+++ Scraping_Projects/quote_game/quote_game.py
@@ -1,4 +1,4 @@
-# Web Scraping Project
+# First Web Scraping Project
 # Introduction
 # In this project you'll be building a quotes guessing game. When run, your program will scrape a website for a collection of quotes. Pick one at random and display it. The player will have four chances to guess who said the quote. After every wrong guess they'll get a hint about the author's identity.
 
