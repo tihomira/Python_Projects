ff098286ce2cfe29ffcdc2ab3e9ec145d6262af2
Tihomira Nikolova <tiholova8@gmail.com>
2021-07-23 15:44:32 +0200

    save highest score to a file

--- Snake_Game/snake.py
+++ Snake_Game/snake.py
@@ -19,7 +19,6 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
@@ -27,9 +26,16 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def resett(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
