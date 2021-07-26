cc149ac2ce8ec749cf9c761e96bd53d907e518f4
Tihomira Nikolova <tiholova8@gmail.com>
2021-07-26 17:16:31 +0200

    nato alphabet converter optimized to catch KeyErrors

--- Nato_Alphabet_Project/main.py
+++ Nato_Alphabet_Project/main.py
@@ -1,9 +1,18 @@
 import pandas
 
 data = pandas.read_csv("~/PycharmProjects/Python_Projects/Nato_Alphabet_Project/nato_phonetic_alphabet.csv")
-data = {row.letter:row.code for (index, row) in data.iterrows()}
+data = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_input = input("Type a word: ").upper()
 
-output_list = [data[letter] for letter in user_input]
-print(output_list)
+def generate_phonetic():
+    user_input = input("Type a word: ").upper()
+    try:
+        output_list = [data[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please!")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
